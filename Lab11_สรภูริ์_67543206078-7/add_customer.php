10d9f13d0d3ca2a014add78714e1dfbe1078ecc1
soraphu <soraphu@gmail.com>
2026-03-13 12:24:26 +0700

    fix: lab title

--- Lab11_สรภูริ์_67543206078-7/add_customer.php
+++ Lab11_สรภูริ์_67543206078-7/add_customer.php
@@ -1,56 +1,62 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>LAB8</title>
+    <title>LAB11</title>
 </head>
 <style>
     .title {
         text-align: right;
         height: 30px;
     }
-    input, textarea, select {
+
+    input,
+    textarea,
+    select {
         margin-left: 10%;
     }
+
     button {
         width: 10%;
         height: auto;
     }
 </style>
+
 <body>
     <form action="show_customer.php" method="post">
-        <input hidden name="Customer_id" value="<?php echo $id ?>" >
-        <input hidden name="action" value="add" >
+        <input hidden name="Customer_id" value="<?php echo $id ?>">
+        <input hidden name="action" value="add">
         <h3 style="color: red;">เพิ่มข้อมูลลูกค้า</h3>
         <fieldset>
             <legend><strong>ข้อมูลลูกค้า</strong></legend>
             <table>
                 <tr>
-                    <td class="title" >ชื่อ:</td>
+                    <td class="title">ชื่อ:</td>
                     <td><input type="text" name="Customer_Name" id="new_name" value="<?php echo $user['Customer_Name']; ?>"></td>
                 </tr>
                 <tr>
-                    <td class="title" >นามสกุล:</td>
+                    <td class="title">นามสกุล:</td>
                     <td><input type="text" name="Customer_Lastname" id="new_lastname" value="<?php echo $user['Customer_Lastname']; ?>"></td>
                 </tr>
                 <tr>
-                    <td class="title" >เพศ</td>
+                    <td class="title">เพศ</td>
                     <td>
-                        <input type="radio" name="Customer_Gender" value="Male" <?php echo ($user['Gender'] == 'Male') ? 'checked' : 'unchecked'; ?> > <label for="Gender">ชาย</label>
-                        <input type="radio" name="Customer_Gender" value="Female" <?php echo ($user['Gender'] == 'Female') ? 'checked' : 'unchecked'; ?> > <label for="Customer_Gender">หญิง</label>
+                        <input type="radio" name="Customer_Gender" value="Male" <?php echo ($user['Gender'] == 'Male') ? 'checked' : 'unchecked'; ?>> <label for="Gender">ชาย</label>
+                        <input type="radio" name="Customer_Gender" value="Female" <?php echo ($user['Gender'] == 'Female') ? 'checked' : 'unchecked'; ?>> <label for="Customer_Gender">หญิง</label>
                     </td>
-                </tr>  
+                </tr>
                 <tr>
-                    <td class="title" >วัน-เดือน-ปีเกิด:</td>
+                    <td class="title">วัน-เดือน-ปีเกิด:</td>
                     <td><input type="date" name="Customer_Birthdate" id="new_birthdate" value="<?php echo $user['Birthdate']; ?>"></td>
                 </tr>
                 <tr>
-                    <td class="title" >ที่อยู่:</td>
+                    <td class="title">ที่อยู่:</td>
                     <td><input type="text" name="Customer_Address" id="new_address" value="<?php echo $user['Address']; ?>"></td>
                 </tr>
                 <tr>
-                    <td class="title" >จังหวัด:</td>
+                    <td class="title">จังหวัด:</td>
                     <td>
                         <select name="Customer_Province" id="new_province">
                             <option value="">เลือกจังหวัด</option>
@@ -60,15 +66,15 @@
                     </td>
                 </tr>
                 <tr>
-                    <td class="title" >รหัสไปรษณีย์:</td>
+                    <td class="title">รหัสไปรษณีย์:</td>
                     <td><input type="text" name="Customer_PostalCode" id="new_postalcode" value="<?php echo $user['Zipcode']; ?>"></td>
                 </tr>
                 <tr>
-                    <td class="title" >โทรศัพท์:</td>
+                    <td class="title">โทรศัพท์:</td>
                     <td><input type="text" name="Customer_Phone" id="new_phone" value="<?php echo $user['Telephone']; ?>"></td>
                 </tr>
                 <tr>
-                    <td class="title" >รายละเอียดอื่นๆ:</td>
+                    <td class="title">รายละเอียดอื่นๆ:</td>
                     <td><textarea name="Customer_Details" rows="3" cols="20" id="new_description"><?php echo $user['Customer_Description']; ?></textarea></td>
                 </tr>
             </table>
@@ -76,15 +82,15 @@
                 <legend><strong>Account ของ ลูกค้า</strong></legend>
                 <table>
                     <tr>
-                        <td class="title" >Username:</td>
+                        <td class="title">Username:</td>
                         <td><input type="text" name="Customer_username" id="username"></td>
                     </tr>
                     <tr>
-                        <td class="title" >Password:</td>
+                        <td class="title">Password:</td>
                         <td><input type="password" name="Customer_password" id="user_password"></td>
                     </tr>
                     <tr>
-                        <td class="title" >Confirm Password:</td>
+                        <td class="title">Confirm Password:</td>
                         <td><input type="password" name="Customer_confirm_password" id="user_confirm_password"></td>
                     </tr>
                 </table>
@@ -96,7 +102,7 @@
         </div>
     </form>
     <script>
-        const form = document.querySelector('form') ;
+        const form = document.querySelector('form');
         form.addEventListener('submit', (e) => {
             const name = document.getElementById('new_name').value.trim();
             const lastname = document.getElementById('new_lastname').value.trim();
@@ -124,4 +130,5 @@
         });
     </script>
 </body>
+
 </html>
